97682e84939dc10243157016f88b718a5b9b29cc
kingsam91 <lambetgm@gmail.com>
2020-06-21 18:39:53 +0300

    link to parent component,create publicrepo function in service

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -8,11 +8,13 @@ import { PagesService } from '../pages-service/pages.service';
 })
 export class PagesComponent implements OnInit {
   public samSon;
+  public nguThi;
 
   constructor(private pagesService:PagesService) { }
 
   ngOnInit(): void {
     this.getGitacc();
+    this.getPublicrepos();
   }
 
   getGitacc() {
@@ -25,5 +27,16 @@ export class PagesComponent implements OnInit {
       () => console.log('done loading sams', this.samSon)
     );
   }
+  getPublicrepos() {
+    this.pagesService.getPublicrepos().subscribe(
+      // the first argument is a function which runs on success
+      data => { this.nguThi = data},
+      // the second argument is a function which runs on error
+      err => console.error(err),
+      // the third argument is a function which runs on completion
+      () => console.log('done loading samsrepos', this.nguThi)
+    );
+  }
+
 
 }

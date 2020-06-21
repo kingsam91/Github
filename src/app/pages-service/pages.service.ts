e9dc144b20c9d692efbcc0e8451ea019bb55888b
kingsam91 <lambetgm@gmail.com>
2020-06-21 08:25:28 +0300

    set headers for making http requests

--- src/app/pages-service/pages.service.ts
+++ src/app/pages-service/pages.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 
 @Injectable({
   providedIn: 'root'
